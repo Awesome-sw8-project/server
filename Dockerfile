81dd92875c77e15ebb63ed5e219b223a3a55a701
MrPekar <martin.pekar@outlook.dk>
2021-05-05 14:34:55 +0200

    Server interfacing

--- Dockerfile
+++ Dockerfile
@@ -4,9 +4,7 @@ RUN apt-get update -y && \
     apt-get install -y python-pip python-dev
 RUN pip install --upgrade pip
 
-COPY requirements.txt ./
 COPY ./ ./
-#RUN pip install -r requirements.txt
 
 EXPOSE 52200
 ENTRYPOINT [ "python", "server.py" ]
--- server.py
+++ server.py
@@ -8,13 +8,22 @@ app = Flask(__name__)
 # Entry point.
 @app.route('/', methods = ['POST'])
 def entry():
-    data = parser.parse_json(request.data)
-    estimated_position = None
+    try:
+        data = parser.parse_json(json.dumps(request.get_json()))
+        estimated_position = None
 
-    for element in data:
-        estimated_position = est.estimate(element)
+        for element in data:
+            estimated_position = est.estimate(element)
 
-    return json.dumps({"x": estimated_position[0], "y": estimated_position[1], "z": estimated_position[2]})
+        return json.dumps({"x": estimated_position[0], "y": estimated_position[1], "z": estimated_position[2]})
+    
+    except Exception as exc:
+        return "JSON parse error."
+
+# Error point.
+@app.route('/', methods = ['GET'])
+def error():
+    return "This should have been a POST request."
 
 if (__name__ == "__main__"):
     app.run(host = '0.0.0.0', port = 52200)
